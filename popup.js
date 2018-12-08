c56ccd661030ca075a555bb00554b9190f6ab05a
Ross <rosskrasner2018@u.northwestern.edu>
2018-12-08 21:25:07 +0900

    pulls ui from data

--- popup.js
+++ popup.js
@@ -1,16 +1,18 @@
 var app = angular.module("mainpopup", []);
 console.log("1");
 app.controller("popupCtrl", function($scope, $http) {
-  $scope.passwords = {
-    "password" : "pass123",
-    "username" : "krasner.ross@gmail.com",
-    "url" : "facebook.com"
-  },
-  {
-    "password" : "p!@as2s123",
-    "username" : "rosskrasner",
-    "url" : "twitter.com"
-  }
+  $scope.passwords = [
+      {
+      "password" : "pass!@#$",
+      "username" : "rosskranser",
+      "url" : "facebookc.com"
+    },
+    {
+      "password" : "06@!@#$",
+      "username" : "krasner.ross@gmail.com",
+      "url" : "twitter.com"
+    }
+  ];
   console.log("2");
 
 });
