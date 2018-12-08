7fb5f15d512fb68fd8f34d5c6c2f933c3515a7dd
Ross <rosskrasner2018@u.northwestern.edu>
2018-12-08 23:56:56 +0900

    progress

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 var app = angular.module("mainpopup", []);
 console.log("1");
-app.controller("popupCtrl", function($scope, $http) {
+app.controller("popupCtrl", function($scope, $http, $window) {
+
+  console.log("2");
+
   $scope.passwords = [
       {
       "password" : "pass!@#$",
@@ -13,6 +16,68 @@ app.controller("popupCtrl", function($scope, $http) {
       "url" : "twitter.com"
     }
   ];
-  console.log("2");
+  $scope.selectedPassword = {};
+
+  $scope.showDetailsForPassword = function(pass) {
+        var pass = JSON.stringify(pass);
+        localStorage.setItem("pass", pass);
+        $window.open('/details.html');
+    };
+
+    $scope.action = function(pass, arg) {
+
+      if (arg === 1) {
+        // Copy Username
+        var pass = localStorage.getItem("pass");
+        var parsed = JSON.parse(pass);
+        var username = parsed.username;
+
+        var input = document.getElementById("copyfrom");
+        input.value = username;
+        input.select();
+        document.execCommand("copy");
+        console.log(username);
+
+      } else if (arg == 2) {
+        // Copy Password
+        var pass = localStorage.getItem("pass");
+        var parsed = JSON.parse(pass);
+        var pw = parsed.password;
+
+        var input = document.getElementById("copyfrom");
+        input.value = pw;
+        input.select();
+        document.execCommand("copy");
+        console.log(pw);
+
+
+      } else if (arg == 3) {
+        // Copy URL
+        var pass = localStorage.getItem("pass");
+        var parsed = JSON.parse(pass);
+        var url = parsed.url;
+
+        var input = document.getElementById("copyfrom");
+        input.value = url;
+        input.select();
+        document.execCommand("copy");
+        console.log(url);
+
+      } else if (arg == 4) {
+        // Go to URL
+        var pass = localStorage.getItem("pass");
+        var parsed = JSON.parse(pass);
+        var url = parsed.url;
+
+        window.location.assign(url)
+
+      } else if (arg == 5) {
+        // Edit
+
+      } else if (arg == 6) {
+        // Delete
+
+      }
+    };
 
 });
